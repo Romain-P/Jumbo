0e5dc0a623064eefb2048af34633b406e78b1da1
John-r <skryn01@gmail.com>
2014-09-02 19:42:09 +0200

    Creating two binding annotations for game & realm network

---
+++ jumbo-login/src/main/java/org/jumbo/utils/annotations/GameNetwork.java
@@ -0,0 +1,17 @@
+package org.jumbo.utils.annotations;
+
+/**
+ * Created by Return on 02/09/2014.
+ */
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@BindingAnnotation
+@Target(ElementType.LOCAL_VARIABLE)
+public @interface GameNetwork {}
