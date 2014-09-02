ebab00232a6b12c2eaeced93cc0100bfb6adb91f
John-r <skryn01@gmail.com>
2014-09-02 04:48:51 +0200

    Creating injector & writing an example of the Slf4j-API

--- jumbo-login/src/main/java/org/jumbo/login/Main.java
+++ jumbo-login/src/main/java/org/jumbo/login/Main.java
@@ -1,7 +1,13 @@
 package org.jumbo.login;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        
+        log.info("Login-server starting..");
+        Injector injector = Guice.createInjector(new LoginDefaultModule());
     }
 }
