4de22cf2eb9594fee8601f29308a07be54204292
John-r <skryn01@gmail.com>
2014-09-03 23:21:04 +0200

    Adding missed element in DataService interface

--- jumbo-api/src/main/java/org/jumbo/api/login/database/DatabaseService.java
+++ jumbo-api/src/main/java/org/jumbo/api/login/database/DatabaseService.java
@@ -1,7 +1,10 @@
 package org.jumbo.api.login.database;
 
+import org.jumbo.commons.sql.QueryManager;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -10,6 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public interface DatabaseService {
     public Connection getConnection();
     public ReentrantLock getLocker();
+    public Map<Class, QueryManager> getQueryManagers();
 
     public DatabaseService start() throws SQLException;
     public void stop();
