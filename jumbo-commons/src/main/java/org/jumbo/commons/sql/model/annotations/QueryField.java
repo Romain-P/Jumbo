e845f98effe81b30adc57607536254051b57b1d7
John-r <skryn01@gmail.com>
2014-09-03 21:21:50 +0200

    Creating an @Annotation to specify field at "column of table" of the QueryManager

---
+++ jumbo-commons/src/main/java/org/jumbo/commons/sql/model/annotations/QueryField.java
@@ -0,0 +1,16 @@
+package org.jumbo.commons.sql.model.annotations;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Return on 03/09/2014.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@BindingAnnotation
+@Target(ElementType.FIELD)
+public @interface QueryField {}
