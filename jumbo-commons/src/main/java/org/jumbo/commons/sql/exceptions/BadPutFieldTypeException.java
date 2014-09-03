86ad74a17b86b42c9057f49928e015d42a68b363
John-r <skryn01@gmail.com>
2014-09-03 21:25:02 +0200

    Creating an exception for bad put types in QueryModel

---
+++ jumbo-commons/src/main/java/org/jumbo/commons/sql/exceptions/BadPutFieldTypeException.java
@@ -0,0 +1,10 @@
+package org.jumbo.commons.sql.exceptions;
+
+/**
+ * Created by Return on 03/09/2014.
+ */
+public class BadPutFieldTypeException extends Exception {
+    public BadPutFieldTypeException(String s) {
+        super(s);
+    }
+}
