061e55c1d441f169befb90324a991b568eacc536
John-r <skryn01@gmail.com>
2014-09-03 21:26:04 +0200

    Creating an exception for bad query formations

---
+++ jumbo-commons/src/main/java/org/jumbo/commons/sql/exceptions/BadQueryFormationException.java
@@ -0,0 +1,10 @@
+package org.jumbo.commons.sql.exceptions;
+
+/**
+ * Created by Return on 03/09/2014.
+ */
+public class BadQueryFormationException extends Exception{
+    public BadQueryFormationException(String s) {
+        super(s);
+    }
+}
