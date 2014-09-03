0276f9e3065a7795125907ee8246ec03fa8ac40b
John-r <skryn01@gmail.com>
2014-09-03 21:37:34 +0200

    Adding DatabaseModule to the main injector

--- jumbo-login/src/main/java/org/jumbo/login/Main.java
+++ jumbo-login/src/main/java/org/jumbo/login/Main.java
@@ -2,11 +2,12 @@ package org.jumbo.login;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.jumbo.backend.database.DatabaseModule;
 import org.jumbo.backend.network.NetworkModule;
 
 public class Main {
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(new LoginDefaultModule(), new NetworkModule());
+        Injector injector = Guice.createInjector(new LoginDefaultModule(), new NetworkModule(), new DatabaseModule());
         final LoginManager manager = injector.getInstance(LoginManager.class).start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
