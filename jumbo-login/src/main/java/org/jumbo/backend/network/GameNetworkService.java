f4b2b58c29d857234b2b4598d625647db3d81bcd
John-r <skryn01@gmail.com>
2014-09-04 19:32:04 +0200

    Adding missed injectMembers to network service handlers

--- jumbo-login/src/main/java/org/jumbo/backend/network/GameNetworkService.java
+++ jumbo-login/src/main/java/org/jumbo/backend/network/GameNetworkService.java
@@ -1,13 +1,22 @@
 package org.jumbo.backend.network;
 
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import org.apache.mina.core.service.IoHandler;
 import org.jumbo.commons.nio.services.NetworkConnector;
 
 /**
  * Created by Return on 02/09/2014.
  */
 public class GameNetworkService extends NetworkConnector{
+    @Inject
+    Injector injector;
+
     @Override
     protected void configure() {
-        connector.setHandler(new GameNetworkHandler());
+        IoHandler handler = new GameNetworkHandler();
+        injector.injectMembers(handler);
+
+        connector.setHandler(handler);
     }
 }
--- jumbo-login/src/main/java/org/jumbo/frontend/network/LoginNetworkService.java
+++ jumbo-login/src/main/java/org/jumbo/frontend/network/LoginNetworkService.java
@@ -1,5 +1,8 @@
 package org.jumbo.frontend.network;
 
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.textline.LineDelimiter;
 import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
@@ -11,12 +14,19 @@ import java.nio.charset.Charset;
  * Created by Return on 02/09/2014.
  */
 public class LoginNetworkService extends NetworkAcceptor{
+    @Inject
+    Injector injector;
+
     @Override
     protected void configure() {
         acceptor.getFilterChain().addLast("login-codec-filter",
             new ProtocolCodecFilter(
             new TextLineCodecFactory(Charset.forName("UTF8"), LineDelimiter.NUL,
             new LineDelimiter("\n\0"))));
-        acceptor.setHandler(new LoginNetworkHandler());
+
+        IoHandler handler = new LoginNetworkHandler();
+        injector.injectMembers(handler);
+
+        acceptor.setHandler(handler);
     }
 }
