8f38a4e8af073c41dab1b7208806600d5e5f59bc
John-r <skryn01@gmail.com>
2014-09-06 22:05:55 +0200

    Replacing sql injection by a preparedStatement for type "INSERT"

--- jumbo-api/src/main/java/org/jumbo/api/database/model/builders/QueryStringBuilder.java
+++ jumbo-api/src/main/java/org/jumbo/api/database/model/builders/QueryStringBuilder.java
@@ -3,25 +3,28 @@ package org.jumbo.api.database.model.builders;
 import org.jumbo.api.database.model.Query;
 import org.jumbo.api.database.model.QueryModel;
 import org.jumbo.api.database.model.enums.OnlyExecuteQueryEnum;
-import org.jumbo.api.database.model.exceptions.BadQueryFormationException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 /**
  * Created by Return on 06/09/2014.
  */
 public interface QueryStringBuilder {
-    public static String newQuery(Query query, OnlyExecuteQueryEnum type) {
+    public static Statement newQuery(Query query, OnlyExecuteQueryEnum type, Connection connection) throws SQLException {
         Object primary = query.getData().get(query.getModel().getPrimaryKeyName());
         Object primaryKey = primary instanceof String ? "'"+primary+"'" :  primary;
 
         switch(type) {
             case CREATE:
-                return String.format(newCreateQuery(query), primaryKey);
+                return newCreateQuery(query, connection);
             case UPDATE:
-                return String.format(newUpdateQuery(query), primaryKey);
+                return String.format(newUpdateQuery(query, connection), primaryKey);
             case DELETE:
-                return String.format(newDeleteQuery(query.getModel()), primaryKey);
+                return String.format(newDeleteQuery(query.getModel()), connection, primaryKey);
             default:
                 return null;
         }
@@ -32,32 +35,46 @@ public interface QueryStringBuilder {
         return String.format(newLoadQuery(model), primaryKey);
     }
 
-    public static String newCreateQuery(Query query) {
+    public static Statement newCreateQuery(Query query, Connection connection) throws SQLException {
         if(!query.checkFormation()) return null;
 
         Map<String, Object> data = query.getData();
 
         StringBuilder built = new StringBuilder("INSERT INTO ");
-        built.append(query.getModel().getTableName()).append(" VALUES(");
+        built.append(query.getModel().getTableName()).append(" (");
 
         boolean first = true;
-        for(Object value: data.values()) {
+        for(String column: data.keySet()) {
             if(first)
                 first = false;
             else
                 built.append(",");
 
-            if(value instanceof String)
-                built.append("'").append(value).append("'");
+            built.append("`").append(query.getModel().getColumns().get(column).getColumnName()).append("`");
+        }
+        built.append(") VALUES(");
+
+        first = true;
+        for(int i=data.size(); i > 0; i--) {
+            if (first)
+                first = false;
             else
-                built.append(value);
+                built.append(",");
+            built.append("?");
         }
         built.append(");");
 
-        return built.toString();
+        PreparedStatement statement = connection.prepareStatement(built.toString());
+
+        for(int i=0; i < data.size(); i++) {
+            Object value = data.values().toArray()[i];
+            statement.setObject(i+1, value);
+        }
+
+        return statement;
     }
 
-    public static String newUpdateQuery(Query query) {
+    public static String newUpdateQuery(Query query, Statement statement) {
         if(!query.checkFormation()) return null;
 
         Map<String, Object> data = query.getData();
@@ -66,19 +83,13 @@ public interface QueryStringBuilder {
         built.append(query.getModel().getTableName()).append(" SET ");
 
         boolean first = true;
-        for(Map.Entry entry: data.entrySet()) {
+        for(String column: data.keySet()) {
             if(first)
                 first = false;
             else
                 built.append(",");
 
-            built.append("`").append(query.getModel().getColumns().get(entry.getKey()).getColumnName()).append("` = ");
-
-            Object value = entry.getValue();
-            if(value instanceof String)
-                built.append("'").append(value).append("' ");
-            else
-                built.append(value).append(" ");
+            built.append("`").append(query.getModel().getColumns().get(column).getColumnName()).append("` = ?");
         }
 
         built.append("WHERE ").append(query.getModel().getPrimaryKeyName()).append(" = %s;");
